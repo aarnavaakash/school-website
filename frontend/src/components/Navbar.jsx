2ec27ad873350a70511519001e3435e92830481f
aarnav aakash <energeticboy@aarnavs-MacBook-Air.local>
2026-05-15 11:17:24 +0530

    improve

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
       </div>
 
       {isOpen && (
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             {navLinks.map((link) => (
               <Link
